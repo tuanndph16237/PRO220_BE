6b5d110676ffc9b929189825f8e9a702c41c6ae6
Ngô Vưn Nam <anh02284@gmail.com>
2022-11-27 04:13:06 +0700

    config app

--- src/app.js
+++ src/app.js
@@ -2,20 +2,45 @@ require('dotenv').config();
 import express from  'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import morgan from 'morgan';
+import ApiError from './utils/ApiError'
 const app = express()
+
+//parse json request body
 app.use(express.json())
+
+//use morgan log info when get data
+app.use(morgan("tiny"))
+
 app.use(cors())
+app.options('*', cors());
+
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+
+// send back a 404 error for any unknown api request
+app.use((req, res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
+})
+
+//use routers
+// app.use('/api', routes);
+
+//conect db 
 try {
   // connect db atlas ket noi db atlas
   ;(async () => {
+    const url =  `mongodb+srv://${process.env.DATABASE}:${process.env.PASSWORD}@cluster0.utwdlzd.mongodb.net/web_app`;
+    console.log('URL',url);
     await mongoose.connect(
-      `mongodb+srv://${process.env.DATABASE}:${process.env.PASSWORD}@cluster0.utwdlzd.mongodb.net/web_app`
+      url
     )
-    console.log('connected db ')
+    console.log('CONNECTED SUCCES DB')
   })()
 } catch (error) {
-  console.log('ket noi that bai')
+console.log('CONNECTED FAILED', error.message)
 }
-const server = app.listen(process.env.PORT, () => {
-  console.log(`connected port ${process.env.PORT}`)
+
+app.listen(process.env.PORT, () => {
+  console.log(`CONNECTED SUCCES PORT ${process.env.PORT}`)
 })
