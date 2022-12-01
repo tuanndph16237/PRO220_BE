e98f3abb514f9fb3786ad4867a564b8603f09505
truongnqph14659 <77135926+truongnqph14659@users.noreply.github.com>
2022-12-02 00:21:41 +0700

    update name: materials to  material

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,4 +1,4 @@
 export * as orderController from './order.controller';
 export * as showroomController from './showroom.controller';
 export * as cateServiceController from './cateService.controller';
-export * as materialsController from './materials.controller';
+export * as materialController from './materials.controller';
