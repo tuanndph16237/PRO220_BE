372fa22dca7eb13762c864426482162b6eafd044
Ngo Van Nam <85476745+namnvph16599@users.noreply.github.com>
2022-12-06 21:32:06 +0700

    feature/remove-banner-by-ids (#17)
    * setting mongoose delete
    
    * feat: refactor route banner
    
    * feat: refactor controller banner
    
    * feat: refactor banner
    
    * feat: refactor banner
    
    * feat: refactor validation banner
    
    * reafactor banner removebyid
    
    * setting lodash
    
    * feat: remove banner by ids
    
    * feat: remove banners by ids
    
    * feat: getall banner by filter

--- src/routes/banner.router.js
+++ src/routes/banner.router.js
@@ -9,6 +9,7 @@ router.get('/banner', BannerController.getAll);
 router.get('/banner/:id', validate(BannerValidation.getById), BannerController.getById);
 router.post('/banner', validate(BannerValidation.create), BannerController.create);
 router.delete('/banner/:id', validate(BannerValidation.getById), BannerController.removeById);
+router.delete('/banner', validate(BannerValidation.deleteByIds), BannerController.removeByIds)
 router.patch(
     '/banner/:id',
     validate(BannerValidation.create),
--- src/services/banner.service.js
+++ src/services/banner.service.js
@@ -1,8 +1,9 @@
+import _ from 'lodash';
 import mongoose from 'mongoose';
 import { BannerModel } from '../models';
 
-export const getAll = async () => {
-    return BannerModel.find({ deleted: false });
+export const getAll = async (filter = { deleted: false }) => {
+    return BannerModel.find({ ...filter });
 };
 
 export const getById = async (_id, filter = { deleted: false }) => {
@@ -13,9 +14,20 @@ export const create = async (data) => {
     return await new BannerModel(data).save();
 };
 
-export const removeById = async (_id) => {
+export const removeById = async (_id, filter = { deleted: false }) => {
     const bannerId = mongoose.Types.ObjectId(_id);
-    return BannerModel.delete({ _id: bannerId });
+    return BannerModel.delete({ _id: bannerId, ...filter });
+};
+
+export const removeByIds = async (ids = []) => {
+    const result = ids.map(async (id) => {
+        //get ra data byid deleted : false just handle
+        const bannerById = await getById(id);
+        if (!_.isEmpty(bannerById)) {
+            removeById(id);
+        }
+    });
+    return result;
 };
 
 export const updateById = async (_id, data) => {
