0618d6f902eac76c2265d5772e7aee9fa44c2564
huynguyen <77097075+huynguyen0353080812@users.noreply.github.com>
2023-02-14 11:48:03 +0700

    fix:paymentVNpay (#49)

---
+++ src/routes/paymentVnpay.router.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import { Payment, vnpayReturn, vnpay_Ipn } from '../controllers/paymentVNPay.controller';
+
+const routerPayment = Router();
+
+routerPayment.post('/create_payment_url', Payment);
+routerPayment.get('/vnpay_return', vnpayReturn);
+routerPayment.get('/vnpay_ipn', vnpay_Ipn);
+
+export default routerPayment;
