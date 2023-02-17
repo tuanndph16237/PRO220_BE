99531f868624ba3558b8e5642e9656a409cf7f17
huynguyen <77097075+huynguyen0353080812@users.noreply.github.com>
2023-02-17 23:09:04 +0700

    Fix: Email (#53)
    * feature: Send Mail
    
    * fix:Email

--- src/controllers/paymentVNPay.controller.js
+++ src/controllers/paymentVNPay.controller.js
@@ -1,9 +1,10 @@
 import dateFormat from 'dateformat';
 import queryStrings from 'qs';
 import crypto from 'crypto';
+import nodemailer from 'nodemailer';
 
 export const Payment = async (req, res) => {
-    const { amount, bankCode, orderInfo, orderType, locale = 'vn' } = req.body;
+    const { amount, bankCode, orderInfo, orderType, locale = 'vn',idOrder } = req.body;
     const ipAddr =
         req.headers['x-forwarded-for'] ||
         req.connection.remoteAddress ||
@@ -24,14 +25,14 @@ export const Payment = async (req, res) => {
     vnp_Params['vnp_OrderInfo'] = orderInfo;
     vnp_Params['vnp_OrderType'] = orderType;
     vnp_Params['vnp_Amount'] = +amount * 100;
-    vnp_Params['vnp_ReturnUrl'] = process.env.VNP_RETURNURL;
-    vnp_Params['vnp_IpAddr'] = ipAddr;
+    vnp_Params['vnp_ReturnUrl'] = `http://localhost:3000/admin/don-hang/${idOrder}`;
+    vnp_Params['vnp_IpAddr'] = '14.248.101.1';
     vnp_Params['vnp_CreateDate'] = createDate;
     if (bankCode !== null && bankCode !== '') {
         vnp_Params['vnp_BankCode'] = bankCode;
     }
     vnp_Params = sortObject(vnp_Params);
-    console.log('vnp_Params', vnp_Params);
+    // console.log('vnp_Params', vnp_Params);
     const signData = queryStrings.stringify(vnp_Params, { encode: false });
     const hmac = crypto.createHmac('sha512', process.env.VNP_HASHSECRET);
     const signed = hmac.update(new Buffer(signData, 'utf-8')).digest('hex');
@@ -83,6 +84,93 @@ export const vnpay_Ipn = (req, res, next) => {
     } else {
         res.status(200).json({ RspCode: '97', Message: 'Fail checksum' });
     }
+};
+
+export const SendMail = async (req, res) => {
+    console.log(req.body);
+    
+    // let transporter = nodemailer.createTransport({
+    //     host: 'smtp.gmail.com',
+    //     port: 587,
+    //     secure: false,
+    //     auth: { 
+    //         user: 'huyndph14652@fpt.edu.vn',
+    //         pass: 'upphlixzjrebbmxz',
+    //     },
+    //     tls: {
+    //         rejectUnauthorized: false,
+    //     },
+    // });
+    // const option = {
+    //     from: 'huyndph14652@fpt.edu.vn',
+    //     to: 'huynguyenz2002@gmail.com',
+    //     subject: 'ok',
+    //     html: ` 
+    //         <div style="width: 100%; background: #f2f2f2; position: relative; padding: 30px">
+    //             <div style="padding: 20px; width: 600px; background: white">
+    //                 <div>
+    //                     <div style="display: flex">
+    //                         <img
+    //                             style="width: 50px"
+    //                             src="https://res.cloudinary.com/df6zs06nu/image/upload/v1676394303/myLogo_nxb70g.png"
+    //                             alt=""
+    //                         />
+    //                         <p style="margin-left: 10px; font-weight: 800; color: #02b875; font-family: sans-serif">
+    //                             DODORIS
+    //                         </p>
+    //                     </div>
+    //                     <p>- Xin chào quý khách,</p>
+    //                     <p>
+    //                         - Dịch vụ sửa xe Dodoris rất vui thông báo đơn hàng #479905473 của quý khách đã được giao thành
+    //                         công ngày 02/02/2023.
+    //                     </p>
+    //                     <p>- Dịch vụ : Bảo dưỡng tại cửa hàng</p>
+    //                     <p>- Thông tin chi tiết đơn hàng :</p>
+    //                 </div>
+    //                 <table border="1">
+    //                     <thead>
+    //                         <tr>
+    //                             <th><span style="padding: 10px;display: block;">STT</span></th>
+    //                             <th><span style="padding: 10px;display: block;">Tên Sản Phẩm</span></th>
+    //                             <th><span style="padding: 10px;display: block;">Số lượng</span></th>
+    //                             <th><span style="padding: 10px;display: block;">Giá</span></th>
+    //                             <th><span style="padding: 10px;display: block;">Tổng Giá</span></th>
+    //                         </tr>
+    //                     </thead>
+    //                     <tbody>
+    //                         <tr>
+    //                             <td><span style="padding: 10px;display: block;">1</span></td>
+    //                             <td><span style="padding: 10px;display: block;">Nhông sên dĩa DID Dream AD3 </span></td>
+    //                             <td><span style="padding: 10px;display: block;"> 3 </span></td>
+    //                             <td><span style="padding: 10px;display: block;"> 200.000 </span></td>
+    //                             <td><span style="padding: 10px;display: block;"> 600.000 </span></td>
+    //                         </tr>
+    //                         <tr>
+    //                             <td><span style="padding: 10px;display: block;">2</span></td>
+    //                             <td><span style="padding: 10px;display: block;">Bộ nồi sau tăng tốc Josho1 xe SH, PCX, Air Blade, Lead, Vision, Dylan</span></td>
+    //                             <td><span style="padding: 10px;display: block;"> 2 </span></td>
+    //                             <td><span style="padding: 10px;display: block;"> 100.000 </span></td>
+    //                             <td><span style="padding: 10px;display: block;"> 200.000 </span></td>
+    //                         </tr>
+    //                         <tr>
+    //                             <td colspan="5">
+    //                                 <span style="padding: 10px;display: block;">Tổng Đơn Hàng : 800.000 VNĐ</span>
+    //                             </td>
+    //                         </tr>
+    //                     </tbody>
+    //                 </table>
+    //                 <p>- Cảm ơn quý khách đã tin tưởng dịch vụ sửa chữa DODORIS</p>
+    //             </div>
+    //         </div>
+    //     `,
+    // };
+    // return transporter.sendMail(option, (err, info) => {
+    //     if (err) {
+    //         return console.log(err);
+    //     }
+
+    //     res.json({ status: info.response });
+    // });
 };
 
 function sortObject(obj) {
